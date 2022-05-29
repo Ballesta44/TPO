db39c44ce9821b77a2f5097aab55b72e987a8db8
Pablo Bellizzi <pbellizzi2@gmail.com>
2022-05-29 20:26:57 -0300

    Agrego contacto

--- scripts.js
+++ scripts.js
@@ -8,18 +8,34 @@ function validoFormulario() {
     let ciudad = document.forms["formulario"]["ciudad"].value
     let telefono = document.forms["formulario"]["telefono"].value
     let email = document.forms["formulario"]["email"].value
+    let letras = new RegExp('^[A-Z]+$', 'i');
+
     if (nombre == "") {
       alert("Debe completar su nombre");
       document.forms["formulario"]["nombre"].focus()
-      return false;
-    }
+      return false;}
+    if (!letras.test(nombre)) {
+        alert("Ingrese un nombre válido");
+        document.forms["formulario"]["nombre"].focus()
+        return false;
+      }
     if (apellido == "") {
         alert("Debe completar su apellido");
         document.forms["formulario"]["apellido"].focus()
         return false;
       }
-      if (pais == "") {
+      if (!letras.test(apellido)) {
+        alert("Ingrese un apellido válido");
+        document.forms["formulario"]["apellido"].focus()
+        return false;
+      }
+    if (pais == "") {
         alert("Debe completar su pais");
+        document.forms["formulario"]["pais"].focus()
+        return false;
+      }
+      if (!letras.test(pais)) {
+        alert("Ingrese un pais válido");
         document.forms["formulario"]["pais"].focus()
         return false;
       }
@@ -28,8 +44,18 @@ function validoFormulario() {
         document.forms["formulario"]["ciudad"].focus()
         return false;
       }
+      if (!letras.test(ciudad)) {
+        alert("Ingrese una ciudad válida");
+        document.forms["formulario"]["ciudad"].focus()
+        return false;
+      }
       if (telefono == "") {
         alert("Debe completar su telefono");
+        document.forms["formulario"]["telefono"].focus()
+        return false;
+      }
+      if (isNaN(telefono)) {
+        alert("Solo ingrese números en el teléfono");
         document.forms["formulario"]["telefono"].focus()
         return false;
       }
